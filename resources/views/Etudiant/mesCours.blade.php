e4f370e4570dbbbb9212d873e65a249b06d88c89
Israe Saidi <israe.2001.saidi@gmail.com>
2022-04-24 00:29:37 +0000

    first commit

---
+++ resources/views/Etudiant/mesCours.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('coursEtudiant')
+<div class="container">
+@if($formationInscrit!==0)
+    <h1>Mes Formations </h1>    
+<div class="row "> 
+   
+    @foreach($formationInscrit as $fr)
+<a href="{{ route('CoursDeFormation',$fr->formation)}}" class="btn btn-warning" style="margin-right: 2vw">Cours de {{$fr->formation}}</a><br>
+@endforeach 
+
+<div class="container">
+<table>
+
+<tr>
+    <th>Formation</th>
+    <th>Horaire</th>
+    <th>Salle</th>
+    <th>Formateur</th>
+    </tr>
+
+@foreach($formationInscrit[0]->sessions as $fr)  
+    <tr>
+      <td>{{$fr->formation}}</td>
+      <td>{{$fr->temps_debut}}-->{{$fr->temps_fin}}</td>
+      <td>{{$fr->salle}}</td>
+      <td>{{$fr->formateur}}</td>
+     
+    </tr> 
+    @endforeach
+  </table>
+
+
+</div>
+</div>
+@else
+Vous n'avez pas encore inscrit dans une formation :(
+@endif
+</div>
+@endsection
